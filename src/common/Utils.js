fe33919c3b8301c44c875beab0410ea63a173768
Tomtfm <61930724+Tomtfm@users.noreply.github.com>
2020-05-07 10:47:52 +0800

    catmall

--- src/common/Utils.js
+++ src/common/Utils.js
@@ -1,10 +1,8 @@
-// 封装防抖函数 debounce   delay ：给定的时间	
+// 封装防抖函数	
 export function debounce(func, delay) {
 	let timer = null
 	return function(...args) {
-		// 判断 timer 是否有值，有的话就把 之前的timer 清除掉 clearTimeout； 
 		if(timer) clearTimeout(timer)
-		// 重新计时：setTimeout
 		timer = setTimeout(() => {
 			func.apply(this, args)
 		}, delay)
@@ -13,7 +11,6 @@ export function debounce(func, delay) {
 
 // 封装 ：对时间进行格式化
 export function formatDate(date, fmt) {
-  // 1.获取年份
 	if (/(y+)/.test(fmt)) {
     fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
   }
